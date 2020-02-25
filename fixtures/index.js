e59a5b7c9f6ab95e18afb510ea30ccd7387ee65d
MO <fengzilong1992@gmail.com>
2020-02-26 02:03:03 +0800

    feat: initialize formatter

---
+++ fixtures/index.js
@@ -0,0 +1,8 @@
+const path = require( 'path' )
+const fs = require( 'fs' )
+const babel = require( 'babel' )
+
+const content = fs.readFileSync( 'index.md', 'utf8' )
+console.log( content )
+
+const { level } = this
