5c676b38c23f542410295bd8e90cd3b27eed6cd4
MO <9125255+fengzilong@users.noreply.github.com>
2025-02-20 02:09:04 +0800

    feat: support JSON/Markdown/CSS highlighting (#34)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,10 @@
-import eslint from '@eslint/js';
+import eslint from '@eslint/js'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import css from '@eslint/css'
 import pluginImportX from 'eslint-plugin-import-x'
 import globals from 'globals'
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 export default [
   {
@@ -10,8 +13,62 @@ export default [
       '**/dist/**',
     ],
   },
-  eslint.configs.recommended,
+
+  // for JSON
   {
+    plugins: {
+      json,
+    },
+  },
+  // lint JSON files
+	{
+		files: ['**/*.json'],
+		ignores: ['package-lock.json'],
+		language: 'json/json',
+		...json.configs.recommended,
+	},
+	// lint JSONC files
+	{
+		files: ['**/*.jsonc'],
+		language: 'json/jsonc',
+		...json.configs.recommended,
+	},
+	// lint JSON5 files
+	{
+		files: ['**/*.json5'],
+		language: 'json/json5',
+		...json.configs.recommended,
+	},
+
+  // for markdown
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown
+    },
+    language: 'markdown/gfm',
+    rules: {
+      'markdown/no-duplicate-headings': 'error',
+      'markdown/heading-increment': 'error',
+    },
+  },
+
+  // for CSS
+  {
+    files: ['**/*.css'],
+    plugins: {
+      css,
+    },
+    language: 'css/css',
+    rules: {
+      'css/no-empty-blocks': 'error',
+      'css/no-invalid-properties': 'error',
+    },
+  },
+  
+  {
+    files: ['**/*.mjs', '**/*.cjs', '**/*.js', '**/*.jsx', '**/*.mts', '**/*.ts', '**/*.tsx'],
+    ...eslint.configs.recommended,
     languageOptions: {
       globals: {
         ...globals.node,
@@ -29,5 +86,5 @@ export default [
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
     },
-  }
+  },
 ]
